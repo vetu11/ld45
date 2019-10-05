b93c1f97cb7f70bd14e3c86b0728faa3766d9cbc
Arget <arget@protonmail.ch>
2019-10-06 00:15:41 +0200

    Ahora con freno de mano incorporado!

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,7 +3,8 @@ extends KinematicBody2D
 enum Directions {UP, DOWN, LEFT, RIGHT}
 
 export(float) var velocity = 100
-
+export(Vector2) var minVelocity = Vector2(30, 30)
+export(int) var minDeltaVelocity = 2
 
 var Vectors = [Vector2(0, -1), Vector2(0, 1), Vector2(-1, 0), Vector2(1, 0)]
 var last_velocity: Vector2
@@ -24,7 +25,11 @@ func _process(delta):
 	
 	if Input.is_action_pressed("right"):
 		input_vector += Vectors[Directions.RIGHT]
-		
+
 	new_velocity += input_vector.normalized() * velocity * delta
-	
+	if (abs(new_velocity.x) < minVelocity.x) && (abs(last_velocity.x - new_velocity.x) < minDeltaVelocity):
+		new_velocity.x = 0
+	if (abs(new_velocity.y) < minVelocity.y) && (abs(last_velocity.y - new_velocity.y) < minDeltaVelocity):
+		new_velocity.y = 0
+	print("X=" + str(new_velocity.x) + "\tY=" + str(new_velocity.y))
 	last_velocity = move_and_slide(new_velocity)
