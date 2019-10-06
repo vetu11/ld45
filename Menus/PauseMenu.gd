9d957fde03360a3d796520960a6cfcd8be37abd9
Arget <arget@protonmail.ch>
2019-10-07 01:49:06 +0200

    Otros cambios menores III

--- Menus/PauseMenu.gd
+++ Menus/PauseMenu.gd
@@ -2,7 +2,8 @@ extends CanvasLayer
 
 # Resume game
 func _on_Button1_pressed():
-	pass
+	get_tree().paused = false
+	self.queue_free()
 
 # Mute music
 func _on_Button2_pressed():
@@ -18,3 +19,8 @@ func _on_Button3_pressed():
 # Exit program
 func _on_Button4_pressed():
 	get_tree().quit()
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		get_tree().paused = false
+		self.queue_free()
