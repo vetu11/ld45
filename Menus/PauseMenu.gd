ef1db7d540215032a055ba4bce6a8a63cac13de8
Arget <arget@protonmail.ch>
2019-10-07 01:38:47 +0200

    Otros cambios menores II

--- Menus/PauseMenu.gd
+++ Menus/PauseMenu.gd
@@ -12,11 +12,9 @@ func _on_Button2_pressed():
 
 # Finish game
 func _on_Button3_pressed():
+	get_tree().paused = false
 	SceneSwitcher.switch("res://Menus/MenuSelector.tscn")
 
 # Exit program
 func _on_Button4_pressed():
 	get_tree().quit()
-
-func _ready():
-	pass
