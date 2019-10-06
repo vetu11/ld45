9c7e016870c765eeb17577926848eaf040405a33
Arget <arget@protonmail.ch>
2019-10-06 23:07:24 +0200

    Linear momentum conservation implemented

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,12 +3,14 @@ extends KinematicBody2D
 enum Directions {UP, DOWN, LEFT, RIGHT}
 
 export(float) var velocity = 100
-export(Vector2) var minVelocity = Vector2(30, 30)
+export(float) var minVelocity = 30
 export(float) var minDeltaVelocity = 0.5
 export(bool) var activate_ground_controls = false
 export(float) var ground_velocity = 300
 export(float) var ground_jump = 600
 export(float) var ground_gravity = 900
+export(float) var airResistance = 0.55
+export(float) var linearMomentumConservation = 0.008
 
 var Vectors = [Vector2(0, -1), Vector2(0, 1), Vector2(-1, 0), Vector2(1, 0)]
 var last_velocity: Vector2
@@ -16,54 +18,56 @@ var last_velocity: Vector2
 func space_controls(delta):
 	var new_velocity := last_velocity
 	var input_vector := Vector2()
-	var animatedSprite = $AnimatedSprite
 	
 	if Input.is_action_pressed("up"):
 		input_vector += Vectors[Directions.UP]
-		
 	if Input.is_action_pressed("down"):
 		input_vector += Vectors[Directions.DOWN]
-	
 	if Input.is_action_pressed("left"):
-		animatedSprite.flip_h = true
+		$AnimatedSprite.flip_h = true
 		input_vector += Vectors[Directions.LEFT]
-	
 	if Input.is_action_pressed("right"):
-		animatedSprite.flip_h = false
+		$AnimatedSprite.flip_h = false
 		input_vector += Vectors[Directions.RIGHT]
 
+	# Calculating new velocity
 	new_velocity += input_vector.normalized() * velocity * delta
-	if (abs(new_velocity.x) < minVelocity.x) && (abs(last_velocity.x - new_velocity.x) < minDeltaVelocity):
+	
+	# If the velocity tends to zero, just make it zero
+	if (abs(new_velocity.x) < minVelocity) && (abs(last_velocity.x - new_velocity.x) < minDeltaVelocity):
 		new_velocity.x = 0
-	if (abs(new_velocity.y) < minVelocity.y) && (abs(last_velocity.y - new_velocity.y) < minDeltaVelocity):
+	if (abs(new_velocity.y) < minVelocity) && (abs(last_velocity.y - new_velocity.y) < minDeltaVelocity):
 		new_velocity.y = 0
+	
+	# Applying new velocity
 	last_velocity = move_and_slide(new_velocity)
 
-
 func ground_controls(delta):
-	
 	var new_velocity := Vector2()
 	new_velocity.y = last_velocity.y + ground_gravity * delta
 	
+	if !is_on_floor(): new_velocity.x = lerp(last_velocity.x, 0, linearMomentumConservation)
 	if Input.is_action_pressed("ground_right"):
 		new_velocity += Vectors[Directions.RIGHT] * ground_velocity
 		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.animation = "walk"		
+		if is_on_floor(): $AnimatedSprite.animation = "walk"
+		else: new_velocity.x *= airResistance
 	elif Input.is_action_pressed("ground_left"):
 		new_velocity += Vectors[Directions.LEFT] * ground_velocity
 		$AnimatedSprite.flip_h = true
-		$AnimatedSprite.animation = "walk"
+		if is_on_floor(): $AnimatedSprite.animation = "walk"
+		else: new_velocity.x *= airResistance
 	else:
 		$AnimatedSprite.animation = "idle"
 	
 	if Input.is_action_pressed("ground_jump") and is_on_floor():
 		new_velocity.y = -ground_jump
+		$AnimatedSprite.animation = "idle"
 	
 	last_velocity = move_and_slide(new_velocity, Vectors[Directions.UP])
 
 
 func _process(delta):
-	
 	if activate_ground_controls:
 		ground_controls(delta)
 	else:
