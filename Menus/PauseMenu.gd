d0fe05343331b6a6dfcf874eba0c1f0a0b0b7e3f
Arget <arget@protonmail.ch>
2019-10-07 23:35:05 +0200

    Sonido en menú de pausa

--- Menus/PauseMenu.gd
+++ Menus/PauseMenu.gd
@@ -7,23 +7,23 @@ func _ready():
 
 # Resume game
 func _on_resume_pressed():
-#	$"Click".play()
+	$"Click".play()
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	get_tree().paused = false
 	self.queue_free()
 	emit_signal("resume")
 
 func _on_mute_pressed():
-#	$"Click".play()
+	$"Click".play()
 	AudioPlayer.mute_unmute()
 
 func _on_main_pressed():
-#	$"Click".play()
+	$"Click".play()
 	get_tree().paused = false
 	SceneSwitcher.switch("res://Menus/MenuSelector.tscn")
 
 func _on_exit_pressed():
-#	$"Click".play()
+	$"Click".play()
 	get_tree().quit()
 
 func _input(event):
