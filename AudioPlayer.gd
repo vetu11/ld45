964b9d737515bbb5a928550966e800e617374143
Arget <arget@protonmail.ch>
2019-10-07 02:09:39 +0200

    Arreglado problema del audio y la pausa

--- AudioPlayer.gd
+++ AudioPlayer.gd
@@ -5,36 +5,17 @@ onready var music_player = preload("res://WorldEnviroment/MusicPlayer.tscn")
 var stream_player: AudioStreamPlayer
 
 func _ready():
-#	var f = File.new()
-#	f.open("user://settings.json", f.WRITE_READ)
-#	var settings = parse_json(f.get_as_text())
-#	f.close()
-#	if typeof(settings) == TYPE_NIL:
-#		settings = {}
-#		settings["mute"] = false
-#
 	stream_player = music_player.instance()
 	self.add_child(stream_player)
-#
-#	if settings["mute"] == false:
-#		stream_player.play()
 
-
+var streamPos
 func mute_unmute():
-	if !stream_player.stream_paused:
-		stream_player.stream_paused = true
+#	if !stream_player.stream_paused:
+#		stream_player.stream_paused = true
+#	else:
+#		stream_player.stream_paused = false
+	if stream_player.playing:
+		streamPos = stream_player.get_playback_position()
+		stream_player.stop()
 	else:
-		stream_player.stream_paused = false
-#	save_settings()
-
-
-#func save_settings():
-#	var f = File.new()
-#	f.open("user://settings.json", f.WRITE_READ)
-#	var settings = parse_json(f.get_as_text())
-#	if typeof(settings) == TYPE_NIL:
-#		settings = {}
-#	settings["mute"] = stream_player.playing
-#	f.seek(0)
-#	f.store_string(str(settings))
-#	f.close()
+		stream_player.play(streamPos)
--- Menus/PauseMenu.gd
+++ Menus/PauseMenu.gd
@@ -1,23 +1,23 @@
 extends CanvasLayer
 
 # Resume game
-func _on_Button1_pressed():
+func _on_resume_pressed():
 	get_tree().paused = false
 	self.queue_free()
 
 # Mute music
-func _on_Button2_pressed():
+func _on_mute_pressed():
 	AudioPlayer.mute_unmute()
 
 ### Botón para mutear todos los sonidos? ###
 
 # Finish game
-func _on_Button3_pressed():
+func _on_main_pressed():
 	get_tree().paused = false
 	SceneSwitcher.switch("res://Menus/MenuSelector.tscn")
 
 # Exit program
-func _on_Button4_pressed():
+func _on_exit_pressed():
 	get_tree().quit()
 
 func _input(event):
